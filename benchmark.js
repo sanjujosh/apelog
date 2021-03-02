211477822ec1d6ecc26179559a90619a188bf88f
Sanju Josh <sanjujosh@users.noreply.github.com>
2021-03-02 14:13:00 +0000

    Minor bug fix, Added ESLint, Little refactoring

--- benchmark.js
+++ benchmark.js
@@ -1,17 +1,17 @@
 const Logger = require('./index')
 const log = new Logger('benchmark')
 
-const Benchmark = require('benchmark');
-const suite = new Benchmark.Suite();
+const Benchmark = require('benchmark')
+const suite = new Benchmark.Suite()
 
-const nestedObjt = {
-    make: 'Ford',
-    model: 'Mustang',
-    year: 1969,
-    good: 'yeah'
-};
+const nestedObj = {
+  make: 'Ford',
+  model: 'Mustang',
+  year: 1969,
+  good: 'yeah'
+}
 
-let results = []
+const results = []
 
 suite
   .add('normal string', function () {
@@ -21,14 +21,14 @@ suite
     log.info('You', 'can', 'add', 'multiple', 'args')
   })
   .add('Object stringify', function () {
-    log.info('You', 'can', 'add', 'multiple', 'args', 'even an object', nestedObjt)
+    log.info('You', 'can', 'add', 'multiple', 'args', 'even an object', nestedObj)
   })
   .on('cycle', function (e) {
     results.push(String(e.target))
   })
-  .on('complete', function() {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
   })
-  .run();
+  .run()
 
 console.info(results)
